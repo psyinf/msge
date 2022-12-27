0e292b88b952a141daad2441fc4a1692f6927e03
psy_inf <sebastian@smesserschmidt.de>
2022-12-28 00:54:10 +0100

    attempt to correct compiler error

--- src/core/serializers/GmtlJsonSerializer.h
+++ src/core/serializers/GmtlJsonSerializer.h
@@ -17,17 +17,17 @@ inline void from_json(const nlohmann::json& j, VecBase<DATA_TYPE, S>& p)
     j.get_to(p.mData);
 }
 
-
+/* FIXME: Compiler error on GCC 12
 inline void to_json(nlohmann::json& j, const Quatd& q)
 {
-    j = nlohmann::json::array({q.mData});
+    j = nlohmann::json::array({*    q.getData()});
 }
 
 inline void from_json(const nlohmann::json& j, Quatd& q)
 {
     j.get_to(q.mData);
 }
-
+*/
 template <typename DATA_TYPE, typename ROTATION_ORDER>
 inline void to_json(nlohmann::json& j, const EulerAngle < DATA_TYPE, ROTATION_ORDER> &e)
 {
